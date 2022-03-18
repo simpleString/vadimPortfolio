da70efe5fadeef9ca704c52f0c702007b3ed5d24
Dmitry Volkov <hot-gun@mail.ru>
2022-03-18 14:19:34 +0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(1000);
 
     return (
         <div className="App">
